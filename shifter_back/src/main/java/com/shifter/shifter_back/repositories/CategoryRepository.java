940d5ad3e10eed5680e9b4aa83aba4c44978880b
Mr TCHOUNGA <jordan.tchounga@2025.ucac-icam.com>
2025-01-23 18:44:15 +0100

    feat: repository okay

---
+++ shifter_back/src/main/java/com/shifter/shifter_back/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.shifter.shifter_back.repositories;
+
+import com.shifter.shifter_back.models.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long>, JpaSpecificationExecutor<Category> {
+}
---
+++ shifter_back/src/main/java/com/shifter/shifter_back/repositories/QuestionRepository.java
@@ -0,0 +1,10 @@
+package com.shifter.shifter_back.repositories;
+
+import com.shifter.shifter_back.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long>, JpaSpecificationExecutor<Question> {
+}
